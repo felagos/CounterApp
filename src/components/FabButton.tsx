2041590b2e63e0188789ba354ea8cebec156f860
Felipe Lagos <felagossanchez@gmail.com>
2021-09-12 21:13:21 -0300

    add fab button component

--- src/components/FabButton.tsx
+++ src/components/FabButton.tsx
@@ -27,6 +27,15 @@ const styles = StyleSheet.create({
 		width: 60,
 		height: 60,
 		justifyContent: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 4,
+		},
+		shadowOpacity: 0.32,
+		shadowRadius: 5.46,
+
+		elevation: 9,
 	},
 	fabItem: {
 		color: 'white',
